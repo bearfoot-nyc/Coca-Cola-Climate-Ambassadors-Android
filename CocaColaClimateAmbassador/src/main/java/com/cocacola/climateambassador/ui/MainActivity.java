d57d194d3277db01a54538a26cc30a24ad873791
Andy Lawton <realandylawton@gmail.com>
2013-08-21 23:15:39 -0400

    Injecting timber

--- CocaColaClimateAmbassador/src/main/java/com/cocacola/climateambassador/ui/MainActivity.java
+++ CocaColaClimateAmbassador/src/main/java/com/cocacola/climateambassador/ui/MainActivity.java
@@ -12,17 +12,13 @@ import timber.log.Timber;
 
 public class MainActivity extends CaActivity {
 
+    @Inject Timber Log;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+        Log.d("Injection status: %s", "live");
     }
     
 }
