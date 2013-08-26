11116d648ca3f145f64d2bb183e83fdd05a6e23a
Andy Lawton <realandylawton@gmail.com>
2013-08-25 20:53:17 -0400

    Files are copying to app directory.  Wrote some new tests

--- CocaColaClimateAmbassador/src/main/java/com/cocacola/climateambassador/AppPackageFileWriter.java
+++ CocaColaClimateAmbassador/src/main/java/com/cocacola/climateambassador/AppPackageFileWriter.java
@@ -3,10 +3,14 @@ package com.cocacola.climateambassador;
 import android.content.Context;
 import android.content.res.AssetManager;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -24,10 +28,18 @@ public class AppPackageFileWriter {
     Timber Log;
 
     private Context mContext;
+    private File mPkgDir;
+    private File mDocsDir;
+    private AssetManager mAssetManager;
+
+    private static final String DOCS_DIR = "docs";
 
     @Inject
     public AppPackageFileWriter(Context context) {
         mContext = context;
+        mPkgDir = context.getFilesDir();
+        mDocsDir = new File(mPkgDir.getAbsolutePath() + File.separator + DOCS_DIR);
+        mAssetManager = context.getAssets();
     }
 
     public void writeAssetsToPackageDir() {
@@ -35,37 +47,60 @@ public class AppPackageFileWriter {
         // Get path of assets
         String[] assets = getAllAssets();
 
-        // Get access to private package dir
-        File pkgDir = mContext.getFilesDir(); // /data/data/com.cocacola.climateambassador/files
-
         // Open each file
         for(String asset : assets) {
 
             // Write file to private package dir
-            writeToPkgDir(pkgDir, asset);
+            writeToPkgDir(createInputFromAsset(DOCS_DIR, asset), mDocsDir, asset);
 
         }
 
     }
 
-    private void writeToPkgDir(File pkgDir, String fileName) {
+    private InputStream createInputFromAsset(String directory, String fileName) {
 
-        String docsPath = pkgDir.getAbsolutePath() + File.separator + "docs";
+        BufferedInputStream in = null;
 
-        File docsDir = new File(docsPath);
-        if (!docsDir.exists()) {
-            docsDir.mkdirs();
+        try {
+            in = new BufferedInputStream(mAssetManager.open(directory + File.separator + fileName));
+        } catch (FileNotFoundException e) {
+            Log.e(e, "No file found for %s", fileName);
+        } catch (IOException e) {
+           Log.e(e, "IOException");
         }
 
-        File file = new File(docsDir, fileName);
+        return in;
+
+    }
+
+    private void writeToPkgDir(InputStream in, File directory, String fileName) {
+
+        // Create the directory if it doesn't exist
+        if (!directory.exists()) {
+            directory.mkdirs();
+        }
+
+        File file = new File(directory, fileName);
+
+        FileOutputStream out = null;
 
         try {
 
-            BufferedOutputStream out;
-            out = new BufferedOutputStream(new FileOutputStream(file));
+            out = new FileOutputStream(file);
+
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = in.read(buffer)) != -1)
+            {
+                out.write(buffer, 0, bytesRead);
+            }
+
+            in.close();
+            in = null;
 
             out.flush();
             out.close();
+            out = null;
 
 
         } catch (IOException e) {
