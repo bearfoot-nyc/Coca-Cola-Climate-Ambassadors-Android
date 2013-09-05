40abb239602e7f58c04dbcd0f30ae68ee4963784
Andy Lawton <andy@socialradar.com>
2013-09-04 22:03:29 -0400

    Created adapter for CaseStudiesListFragment.  Launching CaseActivity that is specificied in getCaseStudyListItems

---
+++ CocaColaClimateAmbassador/src/main/java/com/cocacola/climateambassador/ui/CaseActivity.java
@@ -0,0 +1,9 @@
+package com.cocacola.climateambassador.ui;
+
+/**
+ * Created by realandylawton on 9/4/13.
+ */
+public class CaseActivity extends CaActivity {
+
+
+}
--- CocaColaClimateAmbassador/src/main/java/com/cocacola/climateambassador/ui/CaseStudiesListFragment.java
+++ CocaColaClimateAmbassador/src/main/java/com/cocacola/climateambassador/ui/CaseStudiesListFragment.java
@@ -1,13 +1,18 @@
 package com.cocacola.climateambassador.ui;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import com.cocacola.climateambassador.R;
 
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -15,18 +20,21 @@ import java.util.List;
  */
 public class CaseStudiesListFragment extends CaFragment {
 
+    private CaseStudiesAdapter mAdapter;
+
     private ListView mListView;
+    private List<CaseStudyListItem> mCaseStudyListItems;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mAdapter = new CaseStudiesAdapter(getActivity(), getCaseStudyListItems());
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View v = inflater.inflate(R.layout.frag_case_studies_list, container, true);
-
         return v;
     }
 
@@ -34,7 +42,107 @@ public class CaseStudiesListFragment extends CaFragment {
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         mListView = (ListView) view.findViewById(R.id.case_studies_list_view);
+        mListView.setAdapter(mAdapter);
     }
 
+    private List<CaseStudyListItem> getCaseStudyListItems() {
 
+        if(mCaseStudyListItems == null) {
+            mCaseStudyListItems = new LinkedList<CaseStudyListItem>();
+            mCaseStudyListItems.add(new CaseStudyListItem(0, "Ingredients", CaseActivity.class));
+            mCaseStudyListItems.add(new CaseStudyListItem(0, "Packaging", CaseActivity.class));
+            mCaseStudyListItems.add(new CaseStudyListItem(0, "Manufacturing", CaseActivity.class));
+            mCaseStudyListItems.add(new CaseStudyListItem(0, "Distribution", CaseActivity.class));
+            mCaseStudyListItems.add(new CaseStudyListItem(0, "Refrigeration", CaseActivity.class));
+        }
+
+        return mCaseStudyListItems;
+    }
+
+    private class CaseStudiesAdapter extends BaseAdapter {
+
+        private List<CaseStudyListItem> mItems;
+
+        private Context mContext;
+        private LayoutInflater mInflater;
+
+        private CaseStudiesAdapter(Context context, List<CaseStudyListItem> items) {
+            mItems = items;
+            mContext = context;
+            mInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+
+        @Override
+        public int getCount() {
+            return mItems.size();
+        }
+
+        @Override
+        public CaseStudyListItem getItem(int position) {
+            return mItems.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            View v = mInflater.inflate(R.layout.case_studies_list_item, null);
+
+            final CaseStudyListItem item = getItem(position);
+
+            Button title = (Button) v.findViewById(R.id.case_study_list_item_button);
+            title.setText(item.getTitle());
+            title.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(mContext, item.getClazzToLaunch());
+                    mContext.startActivity(intent);
+                }
+            });
+
+            return v;
+
+        }
+    }
+
+    private class CaseStudyListItem {
+
+        private int iconResId;
+        private String title;
+        private Class<? extends CaActivity> clazzToLaunch;
+
+        private CaseStudyListItem(int iconResId, String title, Class<? extends CaActivity> clazzToLaunch) {
+            this.iconResId = iconResId;
+            this.title = title;
+            this.clazzToLaunch = clazzToLaunch;
+        }
+
+        public int getIconResId() {
+            return iconResId;
+        }
+
+        public void setIconResId(int iconResId) {
+            this.iconResId = iconResId;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public Class<? extends CaActivity> getClazzToLaunch() {
+            return clazzToLaunch;
+        }
+
+        public void setClazzToLaunch(Class<? extends CaActivity> clazzToLaunch) {
+            this.clazzToLaunch = clazzToLaunch;
+        }
+    }
 }
