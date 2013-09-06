7d6ce6a91b388a3dd001edc89621ee061db4deb1
Andy Lawton <realandylawton@gmail.com>
2013-09-06 01:46:31 -0400

    Removed list from Internal Trainings Overview.  Populating title/body.  Injecting view and JsonParser properly

--- CocaColaClimateAmbassador/src/main/java/com/cocacola/climateambassador/ui/fragments/InternalTrainingOverviewFragment.java
+++ CocaColaClimateAmbassador/src/main/java/com/cocacola/climateambassador/ui/fragments/InternalTrainingOverviewFragment.java
@@ -7,10 +7,17 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.cocacola.climateambassador.CaConstants;
 import com.cocacola.climateambassador.R;
 import com.cocacola.climateambassador.models.Document;
+import com.cocacola.climateambassador.models.Module;
+import com.cocacola.climateambassador.util.JsonAssetsLoader;
+
+import java.io.IOException;
+
+import javax.inject.Inject;
 
 import butterknife.InjectView;
 import butterknife.Views;
@@ -21,9 +28,14 @@ import butterknife.Views;
 public class InternalTrainingOverviewFragment extends CaFragment {
 
     @InjectView(R.id.title) TextView mTitleView;
-    @InjectView(R.id.body_text) TextView mBodyTextView;
+    @InjectView(R.id.bodyText) TextView mBodyTextView;
     @InjectView(R.id.textFramesLayout) LinearLayout mTextFramesLayout;
     @InjectView(R.id.bulletPointFramesLayout) LinearLayout mBulletPointsFramesLayout;
+
+    @Inject
+    JsonAssetsLoader mJsonAssetsLoader;
+
+    protected Module mModule;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -35,15 +47,33 @@ public class InternalTrainingOverviewFragment extends CaFragment {
 
         View v = inflater.inflate(R.layout.frag_internal_training_overview, container, true);
 
-
         Views.inject(this, v);
 
         return v;
     }
 
+    public Module getModule() {
+
+        if(mModule == null) {
+            try {
+                mModule = mJsonAssetsLoader.parseModuleFromJsonFile("internal_training_overview.json");
+            } catch (IOException e) {
+                Toast.makeText(getActivity(), "Failed to Get Internal Training Overview", Toast.LENGTH_SHORT);
+            }
+        }
+
+        return mModule;
+    }
+
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        Module module = getModule();
+
+        mTitleView.setText(module.getTitle());
+        mBodyTextView.setText(module.getBodyText());
+
     }
 
 }
