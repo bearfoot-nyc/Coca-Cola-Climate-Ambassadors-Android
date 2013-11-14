12f148975d21fb3508f1b4ff42576cd5cfbcef55
Andy Lawton <andy@socialradar.com>
2013-11-13 22:52:46 -0500

    Added Dao project and configured GreenDao in it

---
+++ Dao/src/com/cocacola/climateambassador/dao/CaDaoGenerator.java
@@ -0,0 +1,13 @@
+package com.cocacola.climateambassador.dao;
+
+public class CaDaoGenerator {
+
+    private static final int DB_VERSION = 1;
+
+    public static void main(String[] args) throws Exception {
+
+
+
+    }
+
+}
